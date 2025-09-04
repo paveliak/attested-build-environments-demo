ccd871a5cdb947905de097cdab97028fb8b4aba2
Pavel Iakovenko <paveliak@users.noreply.github.com>
2025-09-03 20:32:15 -0400

    Cleanup
    Signed-off-by: Pavel Iakovenko <paveliak@users.noreply.github.com>

--- scripts/build-linux-vm.sh
+++ scripts/build-linux-vm.sh
@@ -37,7 +37,6 @@ echo Measuring rootfs
 "$SCRIPTPATH"/rootfs-measure-verity.sh
 
 echo Installing enlightened initramfs scripts and generate initramfs
-echo "TMP_DRIVE_PATH=$TMP_DRIVE_PATH"
 TMP_DRIVE_PATH=$(mktemp -d)
 "$SCRIPTPATH"/../initramfs/install.sh
 mkinitramfs -o "$TMP_DRIVE_PATH/initrd-$(uname -r).img"
