36e0421ecfa880e284224d5acb4d9b9bb603dd0d
Pavel Iakovenko <paveliak@github.com>
2025-11-11 19:05:13 -0500

    Refactoring

--- scripts/rootfs-measure-verity.sh
+++ scripts/rootfs-measure-verity.sh
@@ -13,21 +13,21 @@ VERITY_DEVICE=$(sudo blkid /dev/disk/azure/scsi1/lun0* --match-token PARTLABEL=v
 ROOTFS_PARTUUID=$(sudo blkid -s PARTUUID -o value $ROOTFS_DEVICE)
 VERITY_PARTUUID=$(sudo blkid -s PARTUUID -o value $VERITY_DEVICE)
 
-sudo mkdir -p /mnt/root
-sudo mount -o ro $ROOTFS_DEVICE /mnt/root
-
-sudo mkdir -p /mnt/boot
-sudo mount -o ro $BOOT_DEVICE /mnt/boot
-
-sudo mkdir -p /mnt/uefi
-sudo mount $UEFI_DEVICE /mnt/uefi
-
 echo "Setting up Verity for $ROOTFS_DEVICE on $VERITY_DEVICE"
 #sudo veritysetup --verbose --debug format $ROOTFS_DEVICE $VERITY_DEVICE --root-hash-file rootfs.hash
 
 echo "Building UKI"
+sudo mkdir -p /mnt/root
+sudo mkdir -p /mnt/boot
+sudo mkdir -p /mnt/uefi
+
+sudo mount -o ro $ROOTFS_DEVICE /mnt/root
+sudo mount -o ro $BOOT_DEVICE /mnt/boot
+sudo mount $UEFI_DEVICE /mnt/uefi
+
 PROC_CMDLINE="root=PARTUUID=$ROOTFS_PARTUUID ro veritydata=PARTUUID=$ROOTFS_PARTUUID veritytree=PARTUUID=$VERITY_PARTUUID verityhash=$(cat rootfs.hash) verityname=/dev/meow"
 UNAME=$(ls /mnt/root/usr/lib/modules)
+# TODO: sign UKI for the SecureBoot
 sudo ukify build --linux="/mnt/boot/vmlinuz-$UNAME" --initrd="/mnt/boot/initrd.img-$UNAME" --uname=$UNAME --cmdline="$PROC_CMDLINE" --output=/mnt/uefi/EFI/BOOT/BOOTX64.EFI --all
 
 sudo umount /mnt/uefi
