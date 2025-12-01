0ff368ec9725d90c78d89985355f9cf8b8952603
Pavel Iakovenko <paveliak@github.com>
2025-11-30 20:36:09 -0500

    Debugging Actions is like debugging ENIAC

--- build-azure/attest.sh
+++ build-azure/attest.sh
@@ -4,8 +4,10 @@ set -e
 
 SCRIPTPATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P )"
 
-echo "Creating attested VM..."
 ATTEST_VM_NAME="${AZURE_VM_NAME}attest"
+IMAGE_ID=$(az sig image-definition show --resource-group $AZURE_RESOURCE_GROUP --gallery-name $AZURE_GALLERY_NAME --gallery-image-name $AZURE_IMAGE_DEFINITION | jq -r ".id")
+
+echo "Creating attested VM..."
 $SCRIPTPATH/create-vm $ATTEST_VM_NAME $IMAGE_ID TrustedLaunch
 ATTEST_VM_ID=$(az vm show --resource-group $AZURE_RESOURCE_GROUP --name $ATTEST_VM_NAME | jq -r ".id")
 
