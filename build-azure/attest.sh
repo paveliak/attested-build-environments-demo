c1a5ccd57a66fc180e4ae7761a8b3fffa80e7a63
Pavel Iakovenko <paveliak@github.com>
2025-12-26 22:00:26 -0500

    Building attestation client

--- build-azure/attest.sh
+++ build-azure/attest.sh
@@ -4,6 +4,27 @@ set -e
 
 SCRIPTPATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P )"
 
+b64url() {
+  openssl base64 -e -A | tr '+/' '-_' | tr -d '='
+}
+
+echo "Creating attestation provider..."
+ATTEST_PROVIDER_NAME="${AZURE_VM_NAME}maa"
+az attestation create --name $ATTEST_PROVIDER_NAME --resource-group $AZURE_RESOURCE_GROUP --location $AZURE_LOCATION
+ATTEST_URL=$(az attestation show --resource-group $AZURE_RESOURCE_GROUP --name $ATTEST_PROVIDER_NAME | jq -r ".attestUri")
+
+echo "Setting attestation policy..."
+POLICY_B64=$(cat policy.txt | b64url)
+JWT_HEADER="{\"alg\":\"none\",\"typ\":\"JWT\"}"
+JWT_BODY="{\"AttestationPolicy\":\"$POLICY_B64\"}"
+JWT_HEADER_B64=$(echo -n "$JWT_HEADER" | b64url)
+JWT_BODY_B64=$(echo -n "$JWT_BODY" | b64url)
+
+curl -X PUT -f "$ATTEST_URL/policies/AzureGuest?api-version=2022-08-01" \
+  -H "Authorization: Bearer $(az account get-access-token --resource https://attest.azure.net | jq -r '.accessToken')" \
+  -H "Content-Type: application/json" \
+  -d "$JWT_HEADER_B64.$JWT_BODY_B64."
+
 ATTEST_VM_NAME="${AZURE_VM_NAME}attest"
 IMAGE_ID=$(az sig image-definition show --resource-group $AZURE_RESOURCE_GROUP --gallery-name $AZURE_GALLERY_NAME --gallery-image-name $AZURE_IMAGE_DEFINITION | jq -r ".id")
 
